108bf129b5db01a02f09c85f4fc991e0331a3a3e
reshsingh1995@gmail.com <reshsingh1995@gmail.com>
2022-04-19 10:59:42 +0530

    Ability to load data

---
+++ src/test/java/CensusAnalyzerTest.java
@@ -0,0 +1,19 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CensusAnalyzerTest {
+    private static final String INDIA_CENSUS_CSV_PATH = "C:\\Users\\singh\\IdeaProjects\\censusanalyser\\src\\main\\resources\\IndiaStateCensusData.csv";
+
+    @Test
+    public void givenIndianCensusCSVFileReturnsCorrectRecords() {
+        CensusAnalyzer censusAnalyzer = new CensusAnalyzer();
+
+        try {
+            int numOfRecord = censusAnalyzer.loadIndiaCensusData(INDIA_CENSUS_CSV_PATH);
+            Assert.assertEquals(29, numOfRecord);
+
+        } catch (CensusAnalyzerException e) {
+            e.printStackTrace();
+        }
+    }
+}
