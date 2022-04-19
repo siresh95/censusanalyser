76d4b0be78fc25f753276375ca1ead4da6e01f37
reshsingh1995@gmail.com <reshsingh1995@gmail.com>
2022-04-19 11:10:23 +0530

    ability to check number of record matches.

--- src/test/java/CensusAnalyzerTest.java
+++ src/test/java/CensusAnalyzerTest.java
@@ -2,8 +2,10 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CensusAnalyzerTest {
+
     private static final String INDIA_CENSUS_CSV_PATH = "C:\\Users\\singh\\IdeaProjects\\censusanalyser\\src\\main\\resources\\IndiaStateCensusData.csv";
 
+    //UseCase 1.1 Happy
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
         CensusAnalyzer censusAnalyzer = new CensusAnalyzer();
